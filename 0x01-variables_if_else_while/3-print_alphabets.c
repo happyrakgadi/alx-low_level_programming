4c138fa628ab3c16e0da08f3cbcdb2d97ab6415d
Ramotlalane Mpho <ramotlalanempho.rm@gmail.com>
2022-06-05 04:45:28 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+int main ()
+
+{
+
+	    char x ='a';
+
+	        char y ='A';
+
+		    while (x <= 'z')  {
+
+			            putchar(x);
+
+				            x++;
+
+					        }
+
+		         while (y <= 'Z')  {
+
+				         putchar(y);
+
+					         y++;
+
+						     }
+
+
+
+			    putchar('\n');
+
+			        return 0 ;
+
+}
