47304be24571c1625207156ca34230069dce349b
Ramotlalane Mpho <ramotlalanempho.rm@gmail.com>
2022-06-07 09:02:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,36 +2,40 @@
 
 /**
  *
- *  * main - Entry point
+ *  * main - Write a program that prints _putchar, followed by a new line.
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Return: Always 0.
  *
  *     */
+
+
 
 int main(void)
 
 {
 
-		char *sh = "_putchar";
+		_putchar('_');
+
+			_putchar('p');
+
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+											_putchar('\n');
 
 
 
-			while (*sh)
-
-					{
-
-								_putchar(*sh);
-
-										sh++;
-
-											}
-
-				_putchar('\n');
-
-
-
-					return (0);
+												return (0);
 
 }
