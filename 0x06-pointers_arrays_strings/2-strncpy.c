b2041516f572066529d3c5aa22effcc3c61646c3
Ramotlalane Mpho <ramotlalanempho.rm@gmail.com>
2022-06-17 14:53:18 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,40 +1,14 @@
 #include "main.h"
-
+#include <string.h>
 /**
- * _strncpy - Write a function that copies a string, with number of bytes
- * @dest: This is the output dest
- * @src: This is the input source
- * @n: This is the number of bytes to copy
- *
- * Return: Copied string with the number of bytes
+ * _strncpy -> for copying purpose
+ * @dest: param1
+ * @src: param2
+ * @n: param3
+ * Return: string
  */
-
-
-
 char *_strncpy(char *dest, char *src, int n)
-
 {
-
-		int index;
-
-
-
-			for (index = 0; index < n && src[index] != '\0' ; index++)
-
-					{
-
-								dest[index] = src[index];
-
-									}
-
-				for (; index  < n; index++)
-
-						{
-
-									dest[index] = '\0';
-
-										}
-
-					return (dest);
-
+	strncpy(dest, src, n);
+	retun (dest);
 }
