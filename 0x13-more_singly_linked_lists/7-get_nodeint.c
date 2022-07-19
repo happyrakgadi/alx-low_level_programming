e4217a8d84aeaddbd64de69667b76eb4815d4e0d
Ramotlalane Mpho <ramotlalanempho.rm@gmail.com>
2022-07-19 08:35:37 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,8 +10,10 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;	
+	unsigned int i;
+
 	for (i = 0; i < index && head != NULL; i++)
+
 	{
 		head = head->next;
 	}
